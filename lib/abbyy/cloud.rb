4145ae17f1a7d05afc776afc2f53e9135699ae84
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-08-23 18:52:22 +0300

    Fix loading order

--- lib/abbyy/cloud.rb
+++ lib/abbyy/cloud.rb
@@ -11,21 +11,37 @@ module ABBYY
     require_relative "cloud/connection"
     require_relative "cloud/settings"
 
-    Dir[
-      "lib/abbyy/cloud/exceptions/*.rb",
-      "lib/abbyy/cloud/models/*.rb",
-      "lib/abbyy/cloud/operations/*.rb",
-      "lib/abbyy/cloud/namespaces/*.rb"
-    ].each { |f| require File.expand_path(f) }
+    require_relative "cloud/exceptions/argument_error"
+    require_relative "cloud/exceptions/response_error"
+    require_relative "cloud/exceptions/type_error"
+
+    require_relative "cloud/models/direction"
+    require_relative "cloud/models/discount"
+    require_relative "cloud/models/engine"
+    require_relative "cloud/models/error"
+    require_relative "cloud/models/locale"
+    require_relative "cloud/models/price"
+    require_relative "cloud/models/source_segment"
+    require_relative "cloud/models/source_tag"
+    require_relative "cloud/models/transfer_data"
+    require_relative "cloud/models/translation"
+    require_relative "cloud/models/translation_segment"
+    require_relative "cloud/models/unit_price"
+
+    require_relative "cloud/operations/base"
+    require_relative "cloud/operations/engines"
+    require_relative "cloud/operations/prices"
+    require_relative "cloud/operations/translate"
+    require_relative "cloud/operations/translate_segments"
+
+    require_relative "cloud/namespaces/base"
+    require_relative "cloud/namespaces/machine_translations"
+    require_relative "cloud/namespaces/prices"
 
     attr_reader :settings
 
     def mt
       Namespaces::MachineTranslations.new(settings)
-    end
-
-    def orders
-      Namespaces::Orders.new(settings)
     end
 
     def prices
