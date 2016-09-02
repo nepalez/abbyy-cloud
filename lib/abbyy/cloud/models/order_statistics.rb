32510c4d72ef217ffde8d87b3c031b813894976e
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-09-02 14:19:12 +0300

    Implement model for order statistics

---
+++ lib/abbyy/cloud/models/order_statistics.rb
@@ -0,0 +1,16 @@
+require_relative "source_tag"
+
+class ABBYY::Cloud
+  module Models
+    # Statistics for the order translation
+    class OrderStatistics < Struct
+      attribute :documents_count, Types::Coercible::Int
+      attribute :pages_count,     Types::Coercible::Int
+      attribute :words_count,     Types::Coercible::Int
+      attribute :chars_count,     Types::Coercible::Int
+    end
+
+    # Registers type Types::OrderStatistics
+    Types.register_type OrderStatistics
+  end
+end
---
+++ spec/unit/abbyy/cloud/models/order_statistics_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe ABBYY::Cloud::Models::OrderStatistics do
+  let(:data) do
+    {
+      documents_count: 1,
+      pages_count: 1,
+      words_count: 32,
+      chars_count: 103
+    }
+  end
+
+  subject { described_class.new(data) }
+
+  it { is_expected.to be_kind_of ABBYY::Cloud::Struct }
+  its(:to_h) { is_expected.to eq data }
+end
