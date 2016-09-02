a9b1841365bbd599cbac43471bbf3bcc9d01b88c
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-09-02 14:16:15 +0300

    Implement additional string types

--- lib/abbyy/cloud/types.rb
+++ lib/abbyy/cloud/types.rb
@@ -3,15 +3,26 @@ module ABBYY::Cloud::Types
 
   VERSIONS = [0].freeze
   UNIT_TYPES = %w(Chars Words Pages Documents).freeze
+  TRANSLATION_TYPES = %w(mt ht_express ht_professional ht_expert).freeze
+  COST_TYPES = %w(Default SomeDiscounts AllDiscounts).freeze
+  PAYMENT_TYPES = %w(Manual Balance External).freeze
+  STATUSES = %w(New Submitted PaymentRequired Paid InProgress Done Failed
+                Accepted ReworkRequired Canceled).freeze
   DISCOUNT_TYPES = \
     %w(TMTextMatch TMTaggedTextMatch TMHalfContextMatch TMFullContextMatch)
     .freeze
 
   # Gem-specific primitive types
-  Version      = Coercible::Int.constrained(included_in: VERSIONS)
-  UnitType     = Strict::String.constrained(included_in: UNIT_TYPES)
-  DiscountType = Strict::String.constrained(included_in: DISCOUNT_TYPES)
-  Currency     = Strict::String.constrained(format: /\A[A-Z]{3}\z/)
+  Version         = Coercible::Int.constrained(included_in: VERSIONS)
+  UnitType        = Strict::String.constrained(included_in: UNIT_TYPES)
+  DiscountType    = Strict::String.constrained(included_in: DISCOUNT_TYPES)
+  CostType        = Strict::String.constrained(included_in: COST_TYPES)
+  TranslationType = Strict::String.constrained(included_in: TRANSLATION_TYPES)
+  PaymentType     = Strict::String.constrained(included_in: PAYMENT_TYPES)
+  Status          = Strict::String.constrained(included_in: STATUSES)
+  Currency        = Strict::String.constrained(format: /\A[A-Z]{3}\z/)
+  Label           = Strict::String.constrained(format: /\A.{1,80}\z/)
+  FilledArray     = Array.constrained(min_size: 1)
 
   # Registers new coercible type from a struct class
   def self.register_type(struct, as: nil, constructor: :new)
